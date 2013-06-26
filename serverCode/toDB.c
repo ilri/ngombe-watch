fe597567cda8b35f4ea3a370de7781aafaf2b079
Emmanuel Telewa <e.telewa@cgiar.org>
2013-06-26 11:18:11 +0300

    added minimal error checking for the json lines in the received_data.txt

--- serverCode/toDB.c
+++ serverCode/toDB.c
@@ -41,9 +41,9 @@ char * user = "root";
 char * password = "bananaface!";
 char * database = "ILRI_DB";
 char * table_name = "COW_DATA";
+int validateLine(char * line);
 
-main()
-{
+main(){
 	if(readLinesFromFile(of)==1){
 		printf(" Data stored successfully: file deleted\n");
 		remove("received_data.txt");		
@@ -52,86 +52,93 @@ main()
 		printf(" Errors in storing the data: file retained\n");
 	}
 }
-int readLinesFromFile(char * filename)
-{
+
+int validateLine(char * line){
+	int valid=1; int i;
+	for(i=0;i<strlen(line)-1;i++){
+		if(line[i]==':' && ((line[i+1]==',') || (line[i+1])=='}')){
+			valid=0;
+			break;
+		}
+	}
+	return valid;
+}
+int readLinesFromFile(char * filename){
 	FILE *file = fopen(filename, "r");
 	int success =0;
 
-	if (file != NULL)
-	{
+	if (file != NULL){
 		char line [ MAXSIZE ]; /* or other suitable maximum line size */
-		while (fgets(line, sizeof line, file) != NULL) /* read a line */
-		{
-			if (strlen(line) > 1)
-			{
-				//remove the '{' and '}'
-				line[0] = ' ';
-				line[strlen(line) - 2] = '\0';
-				//remove the extra spaces
-				char *ch = line;
-				char *p1 = ch;
-				char *p2 = ch;
-				p1 = ch;
-				while (*p1 != 0)
+		while (fgets(line, sizeof line, file) != NULL){
+			if (strlen(line) > 1){
+				//validate the line before any further processing
+				if(validateLine(line)==1)
 				{
-					if (isspace(*p1) || (*p1) == '\"')
-					{
-						++p1;
+					//remove the '{' and '}'
+					line[0] = ' ';
+					line[strlen(line) - 2] = '\0';
+					//remove the extra spaces
+					char *ch = line;
+					char *p1 = ch;
+					char *p2 = ch;
+					p1 = ch;
+					while (*p1 != 0){
+						if (isspace(*p1) || (*p1) == '\"'){
+							++p1;
+						}
+						else
+							*p2++ = *p1++;
 					}
-					else
-						*p2++ = *p1++;
-				}
-				*p2 = 0;
+					*p2 = 0;
 
-				//now the string without extra spaces is stored in 'ch'
-				//printf("Splitting string \"%s\" into tokens:\n", ch);
-				char * keyV[13];
+					//now the string without extra spaces is stored in 'ch'
+					//printf("Splitting string \"%s\" into tokens:\n", ch);
+					char * keyV[13];
 
-				char *str = line;
-				char * pch;
-				pch = strtok(str, ",");
-				int i = 0;
-				while (pch != NULL)
-				{
-					keyV[i] = pch;
-					pch = strtok(NULL, ",");
-					i++;
-				}
+					char *str = line;
+					char * pch;
+					pch = strtok(str, ",");
+					int i = 0;
+					while (pch != NULL){
+						keyV[i] = pch;
+						pch = strtok(NULL, ",");
+						i++;
+					}
 
-				//now tokenize them further
-				//now each of those tokens is a key-value pair
-				char *values[26];
-				int k, x;
-				x = 0;
-				for (k = 0; k < i; k++)
-				{
-					char *str2 = keyV[k];
-					char * pch2;
-					pch2 = strtok(str2, ":");
-					while (pch2 != NULL)
-					{
-						values[x++] = pch2;
-						pch2 = strtok(NULL, ":");
+					//now tokenize them further
+					//now each of those tokens is a key-value pair
+					char *values[26];
+					int k, x;
+					x = 0;
+					for (k = 0; k < i; k++){
+						char *str2 = keyV[k];
+						char * pch2;
+						pch2 = strtok(str2, ":");
+						while (pch2 != NULL){
+							values[x++] = pch2;
+							pch2 = strtok(NULL, ":");
+						}
+					}
+					//call function to insert to db with the cprrect params
+					success = storeToDB(values[1], values[3], values[5], values[7], values[9], values[11], values[13], values[15], values[17], values[19], values[21], values[23],values[25]);
+					memset(line, 0, sizeof (line));
+					if(success==0){//if it fails any where, break here
+						break;
 					}
 				}
-				//call function to insert to db with the cprrect params
-				success = storeToDB(values[1], values[3], values[5], values[7], values[9], values[11], values[13], values[15], values[17], values[19], values[21], values[23],values[25]);
-				memset(line, 0, sizeof (line));
-                                if(success==0){//if it fails any where, break here
-					break;
+				else {
+					printf("Not a valid json string\n");
 				}
 			}
 		}
 		fclose(file);
 	}
-	else
-	{
+	else{
 		perror(filename); /* why didn't the file open? */
 	}
 	return success;
 }
-char *strrev(char *str)
-{
+char *strrev(char *str){
 	char temp[strlen(str)];
 	strcpy(temp,str);
 	str[0]=temp[4];
@@ -142,16 +149,14 @@ char *strrev(char *str)
 	str[5]=temp[1];
 	return str;
 }
-int storeToDB(char * ax, char *ay, char *az,char *tm, char *dt, char *lt, char *ln, char *al, char *sp, char *cs, char *temp,char *battery, char *battVolt)
-{
+int storeToDB(char * ax, char *ay, char *az,char *tm, char *dt, char *lt, char *ln, char *al, char *sp, char *cs, char *temp,char *battery, char *battVolt){
 	int stored = 0;
 	MYSQL *conn;
 	MYSQL_ROW row;
 	conn = mysql_init(NULL);
 
 	/* Connect to database */
-	if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
-	{
+	if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)){
 		fprintf(stderr, "%s\n", mysql_error(conn));
 		return stored;
 	}
@@ -193,16 +198,13 @@ int storeToDB(char * ax, char *ay, char *az,char *tm, char *dt, char *lt, char *
 	strcat(command, "','");
 	strcat(command, battVolt);
 	strcat(command, "')");
-//	printf("%s\n",command);
-//	exit(0);
+	
 	/* send SQL query */
-	if (mysql_query(conn, command))
-	{
+	if (mysql_query(conn, command)){
 		fprintf(stderr, "%s\n\n", mysql_error(conn));
 		stored = 0;
 	}
-	else
-	{
+	else{
 		printf("Data inserted\n");
 		stored = 1;
 	}
